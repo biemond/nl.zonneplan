581b030c076e383a1bc2556202aff115f41bd2ca
Edwin Biemond <biemond@gmail.com>
2023-06-17 16:22:25 +0200

    add device

--- drivers/inverter/driver.js
+++ drivers/inverter/driver.js
@@ -10,16 +10,31 @@ module.exports = class SolarplanDriver extends Homey.Driver {
 
   async onPair(session) {
 
+    let emailkey = this.homey.settings.get('email')
+    let tempPasskey = this.homey.settings.get('password')
+    console.log("email " + emailkey);
+    console.log("pass " + tempPasskey);
+
     session.setHandler("list_devices", async function () {
-      let devices = [];
-      var device = {
-        "name": 'blabla',
-        "data": {
-          id: 'aa'
-        }
-      };
-      devices.push(device);
-      return devices;
+      if (emailkey == null || tempPasskey == null) {
+        new Error('Please activate email 1st in the app settings!');
+        return [];
+      } else {
+
+        let devices = [];
+
+        var device = {
+          name: "bla",
+          data: {
+            id: "bla",
+            name: "bla"
+          }
+        };
+        devices.push(device);
+
+        console.log(devices);
+        return devices;
+      }
     });
   }
 
