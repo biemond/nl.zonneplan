3b01d55b3967bc638ae9f1d6e3f51f52ab664126
Edwin Biemond <biemond@gmail.com>
2023-07-10 20:13:10 +0200

    device added

--- drivers/inverter/device.js
+++ drivers/inverter/device.js
@@ -2,28 +2,54 @@
 
 const Homey = require('homey');
 
+const RETRY_INTERVAL = 300 * 1000;
+let timer;
+
 module.exports = class SolarplanDevice extends Homey.Device {
 
   async onInit() {
     this.log('SolarplanDevice has been inited');
+
+    let name = this.getData().id;
+    this.log("device name id " + name);
+    this.log("device name " + this.getName());
+
+    this.pollInvertor();
+
+    timer = this.homey.setInterval(() => {
+      // poll device state from inverter
+      this.pollInvertor();
+    }, RETRY_INTERVAL);
+
+    this.pollInvertor();
   }
 
-async onAdded() {
+  async onAdded() {
     this.log('SolarplanDevice has been added');
-}
+  }
 
-async onSettings({ oldSettings: { }, newSettings: { }, changedKeys: { } }) {
+  async onSettings({ oldSettings: { }, newSettings: { }, changedKeys: { } }) {
     this.log('SolarplanDevice settings where changed');
-}
+  }
 
-async onRenamed(name) {
+  async onRenamed(name) {
     this.log('SolarplanDevice was renamed');
-}
+  }
 
-async onDeleted() {
+  async onDeleted() {
     this.log('SolarplanDevice has been deleted');
+    this.homey.clearInterval(timer);
+  } // end onDeleted
 
-} // end onDeleted
+  async pollInvertor() {
+    this.log("pollInvertor");
 
+    let accessToken = this.homey.settings.get('access_token')
+    let refreshToken = this.homey.settings.get('refresh_token')
 
+    var unitID = this.getData().id;
+    console.log("accessToken " + accessToken);
+    console.log("refreshToken " + refreshToken);
+    console.log("id " + unitID);
+  }
 }
--- drivers/inverter/driver.js
+++ drivers/inverter/driver.js
@@ -4,59 +4,56 @@ const Homey = require('homey');
 const apis = require('./api.js');
 
 module.exports = class SolarplanDriver extends Homey.Driver {
-  
+
   async onInit() {
   }
 
   async onPair(session) {
-    let emailkey = this.homey.settings.get('email')
-    let tempPasskey = this.homey.settings.get('password')
-    console.log("email " + emailkey);
-    console.log("pass " + tempPasskey);
 
     let accessToken = this.homey.settings.get('access_token')
-    let refreshToken = this.homey.settings.get('access_token')
+    let refreshToken = this.homey.settings.get('refresh_token')
     console.log("accessToken " + accessToken);
     console.log("refreshToken " + refreshToken);
-   let dataObject = {}
+    let dataObject = {}
     session.setHandler("list_devices", async function () {
-      if (emailkey == null || tempPasskey == null || accessToken == null || refreshToken == null) {
+      if (accessToken == null || refreshToken == null) {
         new Error('Please activate email 1st in the app settings!');
         return [];
       } else {
 
         let devices = [];
-      
-       const resp = await apis.getDevice(accessToken)
+
+        const resp = await apis.getDevice(accessToken)
         dataObject.contractUUID = getContractUUID(resp.data.address_groups);
-       
-       const id = resp.data.address_groups[0].connections[0].uuid;
-       var device = {
-        name: id ,
-        data: {
-          id: id,
-          name: id
-        }
-      };
+
+        var device = {
+          name: dataObject.contractUUID,
+          data: {
+            id: dataObject.contractUUID,
+            name: dataObject.contractUUID
+          }
+        };
 
         devices.push(device);
 
-       // console.log('List of devices ',devices);
+        // console.log('List of devices ',devices);
         return devices;
       }
     });
   }
 }
 
-function getContractUUID(arrayOfGroups){
+function getContractUUID(arrayOfGroups) {
   const filteredData = arrayOfGroups.map((element) => {
-    return {connections: element.connections.map((connection) =>{
-    return{contracts:connection.contracts.find((contract) => contract.type == 'pv_installation')}
-  } 
-  
-  )
-}})
+    return {
+      connections: element.connections.map((connection) => {
+        return { contracts: connection.contracts.find((contract) => contract.type == 'pv_installation') }
+      }
 
-return filteredData[0].connections[0].contracts.uuid
+      )
+    }
+  })
+
+  return filteredData[0].connections[0].contracts.uuid
 
 }
