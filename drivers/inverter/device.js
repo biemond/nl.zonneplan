c7d854ce3c3b2b4b8c3fead9d00ad2e496af74c3
Edwin Biemond <biemond@gmail.com>
2023-07-11 20:11:57 +0200

    add caps

--- drivers/inverter/device.js
+++ drivers/inverter/device.js
@@ -42,6 +42,13 @@ module.exports = class SolarplanDevice extends Homey.Device {
     this.homey.clearInterval(timer);
   } // end onDeleted
 
+  validResult(entry) {
+    if (entry) {
+      return true;
+    }
+    return false;
+  }
+
   async pollInvertor() {
     this.log("pollInvertor");
 
@@ -55,10 +62,40 @@ module.exports = class SolarplanDevice extends Homey.Device {
     const resp = await apis.getDevice(accessToken)
     const meta = getContractData(resp.data.address_groups, unitID)
     console.log("meta data ", meta)
+    if (this.validResult(meta['last_measured_power_value'])) {
+      this.addCapability('measure_power');
+      var power = meta['last_measured_power_value'];
+      this.setCapabilityValue('measure_power', power);
+    }
+    if (this.validResult(meta['total_power_measured'])) {
+      this.addCapability('meter_power');
+      var power = meta['total_power_measured'];
+      this.setCapabilityValue('meter_power', power);
+    }
+    if (this.validResult(meta['last_measured_at'])) {
+      this.addCapability('lastmeasured');
+      var power = meta['last_measured_at'];
+      this.setCapabilityValue('lastmeasured', power);
+    }  
+    if (this.validResult(meta['panel_wp'])) {
+      this.addCapability('panel_wp');
+      var power = meta['panel_wp'];
+      this.setCapabilityValue('panel_wp', power);
+    }  
+    if (this.validResult(meta['installation_wp'])) {
+      this.addCapability('panel_total_wp');
+      var power = meta['installation_wp'];
+      this.setCapabilityValue('panel_total_wp', power);
+    }  
+    if (this.validResult(meta['panel_count'])) {
+      this.addCapability('panel_count');
+      var power = meta['panel_count'];
+      this.setCapabilityValue('panel_count', power);
+    }                      
   }
 }
 
-function getContractData(arrayOfGroups,id) {
+function getContractData(arrayOfGroups, id) {
   const filteredData = arrayOfGroups.map((element) => {
     return {
       connections: element.connections.map((connection) => {
